430583d4b596c77fb76e1fd4994e781aaed36cde
Sujata Dev <sujatadev97@gmail.com>
2016-12-27 20:23:48 +0530

    printing number

---
+++ src/abc2.java
@@ -0,0 +1,11 @@
+class abc2
+{
+    public static void main(String args[])
+    {
+        int num=100;
+        System.out.println("Num="+num);
+        num=num*2;
+        System.out.print("Num*2=");
+        System.out.println(num);
+    }
+}
