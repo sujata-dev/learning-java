3ea30e40c9ff974a52edfb8a743568e701cfa33f
Sujata Dev <sujatadev97@gmail.com>
2016-12-27 20:36:27 +0530

    Using if statement

---
+++ src/If.java
@@ -0,0 +1,15 @@
+class If
+{
+    public static void main(String args[])
+    {
+        int x=10,y=20;
+        if(x<y)
+            System.out.println("x<y");
+        x*=2;
+        if(x==y)
+            System.out.println("x=y");
+        x*=2;
+        if(x>y)
+            System.out.println("x>y");
+    }
+}
