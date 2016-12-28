e188514b848177c1bd65581aeca3177887ad6d3e
Sujata Dev <sujatadev97@gmail.com>
2016-12-28 21:54:32 +0530

    area of circle

---
+++ src/area_circle.java
@@ -0,0 +1,8 @@
+public class area_circle
+{
+    public static void main(String []args)
+    {
+        double pi=3.14,r=11,a;
+        System.out.print("Area of circle of radius "+r+" is: "+(pi*r*r));
+    }
+}
