0ce059a6995568787b1639f8f909f29509ebec92
Sujata Dev <sujatadev97@gmail.com>
2016-12-29 21:17:02 +0530

    using do while

---
+++ src/DoWhile.java
@@ -0,0 +1,11 @@
+public class DoWhile
+{
+    public static void main(String args[])
+    {
+        int x=10;
+        do
+        {
+            System.out.print("x="+(x++)+"\n");
+        }while(x<20);
+    }
+}
