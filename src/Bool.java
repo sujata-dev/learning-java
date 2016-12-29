172ece2885b32c0e32f64a17082defb7ea6bbca2
Sujata Dev <sujatadev97@gmail.com>
2016-12-29 21:15:54 +0530

    using boolean

---
+++ src/Bool.java
@@ -0,0 +1,11 @@
+public class Bool
+{
+    public static void main(String []args)
+    {
+        boolean b=false;
+        System.out.print("b= "+b+"\n");
+        if(b)
+            System.out.print("Not executed");
+        System.out.print("10>9: "+(10>9));
+    }
+}
