ca1674f1cfba7d984a8099e369943a4aed32f9b2
Sujata Dev <sujatadev97@gmail.com>
2016-12-30 21:49:15 +0530

    printing all array elements

---
+++ src/print_all_ele.java
@@ -0,0 +1,11 @@
+public class print_all_ele
+{
+    public static void main(String args[])
+    {
+        double[] x={1,2,3,4};
+        for(double i:x)
+        {
+            System.out.println(i);
+        }
+    }
+}
