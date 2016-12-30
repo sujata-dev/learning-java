4456d50d48c0276808c019a95d224186958f00d3
Sujata Dev <sujatadev97@gmail.com>
2016-12-30 21:48:17 +0530

    dynamic initialisation of variables

---
+++ src/dynamic_init.java
@@ -0,0 +1,9 @@
+public class dynamic_init
+{
+    public static void main(String args[])
+    {
+        double a=3,b=4;
+        double c=Math.sqrt(Math.pow(a,2.0)+Math.pow(b,2.0));
+        System.out.print("Hypotenuse: "+c);
+    }
+}
