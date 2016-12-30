9b8cdff11856b3ec1464815ba573014395556efd
Sujata Dev <sujatadev97@gmail.com>
2016-12-30 21:47:19 +0530

    allocation of array

---
+++ src/allocating_arrays.java
@@ -0,0 +1,16 @@
+public class allocating_arrays
+{
+    public static void main(String args[])
+    {
+        int i;
+        int arr[]=new int[3];
+        float arr1[]=new float[3];
+        boolean arr2[]=new boolean[3];
+        for(i=0;i<3;i++)
+            System.out.println(arr[i]);
+        for(i=0;i<3;i++)
+            System.out.println(arr1[i]);
+        for(i=0;i<3;i++)
+            System.out.println(arr2[i]);
+    }
+}
