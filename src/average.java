ffb7ee352bdd88b8be0e99f69851f7b9361bfce6
Sujata Dev <sujatadev97@gmail.com>
2017-01-01 00:23:06 +0530

    finding average of values in an array

---
+++ src/average.java
@@ -0,0 +1,10 @@
+public class average
+{
+    public static void main(String args[])
+    {
+        float arr[]={10,11,12,13,14},result=0;
+        for(int i=0;i<5;i++)
+            result=result+arr[i];
+        System.out.println("Average of the value: "+(result/5));
+    }
+}
