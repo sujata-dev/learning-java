e05cd9a68bca9870f1e8454727c4fcd164aedf53
Sujata Dev <sujatadev97@gmail.com>
2017-01-05 01:30:06 +0530

    using different operators of java

---
+++ src/operator.java
@@ -0,0 +1,11 @@
+public class operator
+{
+    public static void main(String args[])
+    {
+        int a=3,b=10;
+        boolean x=true,y=false;
+        System.out.println(a<<2);
+        System.out.println((!x)|y);
+        System.out.println((b==1)?20:30);
+    }
+}
