a9abacedd4da9621ec12662786cd7a85053bcc4d
Sujata Dev <sujatadev97@gmail.com>
2017-01-07 00:38:19 +0530

    finding midpoint of 2 numbers

---
+++ src/midpoint.java
@@ -0,0 +1,9 @@
+public class midpoint
+{
+    public static void main(String args[])
+    {
+        int i=100,j=200;
+        while(++i < --j);
+        System.out.println("Midpoint of i and j: "+i);
+    }
+}
