1b24e1f418226bfa2dfde188f31b16756f42c2c8
Sujata Dev <sujatadev97@gmail.com>
2017-01-08 21:06:18 +0530

    using for-each loop

---
+++ src/For_each.java
@@ -0,0 +1,10 @@
+public class For_each
+{
+    public static void main(String args[])
+    {
+        int nums[]={1,2,3,4};
+        for(int x:nums) System.out.print((x*10)+" ");
+        System.out.println();
+        for(int x:nums) System.out.print(x+" ");
+    }
+}
